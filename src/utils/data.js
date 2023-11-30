16cdd3e2d6d930192c04e096fb62eccc71ca73a0
saepulfariz <saepulfariz108@gmail.com>
2023-12-01 05:25:34 +0700

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: 'Saepul Hidayat',
+      tag: 'saepulfariz',
+      imageUrl: 'https://avatars.githubusercontent.com/u/65041802?v=4',
+    },
+    {
+      id: 2,
+      name: 'Dimas Saputra',
+      tag: 'dimasmds',
+      imageUrl: '/images/dimasmds.jpeg',
+    },
+    {
+      id: 3,
+      name: 'Arif Faizin',
+      tag: 'arifaizin',
+      imageUrl: '/images/arifaizin.jpeg',
+    },
+    {
+      id: 4,
+      name: 'Rahmat Fajri',
+      tag: 'rfajri27',
+      imageUrl: '/images/rfajri27.jpeg',
+    },
+  ];
+};
+
+export { getData };
