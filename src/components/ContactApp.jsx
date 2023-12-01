49d3f0636b21ac808e6f7019203202530c434b2f
saepulfariz <saepulfariz108@gmail.com>
2023-12-02 06:30:46 +0700

    add form input contact

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
+import ContactInput from './ContactInput';
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -10,6 +11,23 @@ class ContactApp extends React.Component {
     };
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
+  }
+
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl: '/images/default.jpg',
+          },
+        ],
+      };
+    });
   }
 
   onDeleteHandler(id) {
@@ -20,7 +38,10 @@ class ContactApp extends React.Component {
   render() {
     return (
       <div className='contact-app'>
-        <h1>Daftar Kontak</h1>
+        <h1>Aplikasi Kontak</h1>
+        <h2>Tambah Kontak</h2>
+        <ContactInput addContact={this.onAddContactHandler} />
+        <h2>Daftar Kontak</h2>
         <ContactList
           contacts={this.state.contacts}
           onDelete={this.onDeleteHandler}
